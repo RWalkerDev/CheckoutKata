8ed37f7cef1f0211f1952b490c9f36f17d2c703a
Ryan Walker <ryan.walker@ultimedia.co.uk>
2022-02-13 16:16:46 +0000

    added failing test for counting the items in the Checkout

---
+++ CheckoutKata.UnitTests/CheckoutTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace CheckoutKata.UnitTests
+{
+    public class CheckoutTests
+    {
+        [Fact]
+        public void Given_there_are_no_items_in_the_basket_the_total_items_count_should_be_zero()
+        {
+            //Arrange
+            var checkout = new Checkout();
+
+            //Act
+            var totalItems = checkout.TotalItems();
+
+            //Assert
+            Assert.Equal(0, totalItems);
+        }
+    }
+}
---
+++ CheckoutKata/Checkout.cs
@@ -0,0 +1,15 @@
+﻿namespace CheckoutKata
+{
+    public class Checkout
+    {
+        public void Scan(string item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int TotalItems()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
